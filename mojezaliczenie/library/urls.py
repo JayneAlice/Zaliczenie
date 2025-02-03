458683e238e075b949e8988390afa850748cd8a6
JayneAlice <amatuszak5@st.swps.edu.pl>
2025-02-03 22:03:31 +0100

    jej ruszyło

---
+++ mojezaliczenie/library/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+# importujemy moduł views (plik views.py z tego samego katalogu co plik bieżący)
+from . import views
+
+# definiujemy zmienną urlpatterns, która jest listą mapowań adresów URL na nasze widoki
+urlpatterns = [
+    path("", views.welcome_view),
+    # path("autors",views.author_list),
+    # path("books",views.book_list),
+    # path("users",views.user_list),
+    # path("categorys",views.category_list),
+    # path("orders",views.order_list),
+]
--- mojezaliczenie/library/views.py
+++ mojezaliczenie/library/views.py
@@ -15,10 +15,10 @@ def welcome_view(request):
         </body></html>"""
     return HttpResponse(html)
 
-# def book_list(request):
-#     books = Book.objects.all()
-#     return render(request, 'book_list.html', 
-#                   {'books': books})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/list.html', 
+                  {'books': books})
 
 # def book_detail(request, book_id):
 #     book = get_object_or_404(Book, id=book_id)
